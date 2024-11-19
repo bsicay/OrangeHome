3b74dd7ac890fe822a98a90be1bd4b1918a1f49c
Brandon Sicay <bsicay@pacifiko.com>
2024-11-19 15:43:04 -0600

    auto play speed

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     fade: true,
     arrows: false,
   }
